10bef01b900b01b402d8bfa9e1123b8344aa723b
Farhat Asharfillah <FarhatAsharfillah>
2024-02-26 15:16:54 +0700

    Add project files.

---
+++ SDSDS/SDSDS.cpp
@@ -0,0 +1,24 @@
+// SDSDS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <istream>
+using namespace std;
+
+int main()
+{//begin
+    // numeric nPanjang, nLebar, nLuas
+    // display 'Masukan Panjang= '
+    // accept nPanjang
+    // display 'Masukan Lebar = '
+    // accept nLebar
+    // compute nLuas = nPanjang * nlebar
+    // display 'Luasnya adalah = ' + nLuas
+
+    int nPanjang, nLebar, nLuas;
+    cout << " Masukan Panjang = ";
+    cin >> nPanjang;
+    cout << "Masukan Lebar = ";
+    cin >> nLebar;
+    nLuas = nPanjang *
+
+}
